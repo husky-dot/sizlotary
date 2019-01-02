5849497ccfaf64ea4a43cc6a206c59c18ee70096
qq449245884 <449245884@qq.com>
2019-01-02 20:50:58 +0800

    initial

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="app-main">
+      <transition name="van-fade">
+        <router-view/>
+      </transition>
+    </div>
+    <div class="tab-bar van-hairline--top">
+      <router-link to="/" tag="div">
+        <span class="tab-label">首页</span>
+      </router-link>
+      <router-link to="/about" tag="div">
+        <span class="tab-label">我的押码</span>
+      </router-link>
+      <router-link to="/about" tag="div">
+        <span class="tab-label">个人中心</span>
+      </router-link>
+    </div>
+
+  </div>
+</template>
+
+<style lang="less">
+  .app-main {
+    height: calc(100vh - 100px)
+  }
+  .tab-bar{
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    div{
+      flex: 1;
+      text-align: center;
+    }
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <!-- <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import VanButton from 'vant/lib/button'
+import 'vant/lib/button/style'
+export default {
+  name: 'home',
+  components: {
+    VanButton
+  }
+}
+</script>
